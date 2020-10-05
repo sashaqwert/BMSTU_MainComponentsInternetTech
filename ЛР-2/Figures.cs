53c2b3a62a84b77e8d937149bab42e547ec52307
Alexandr <admin@sccraft.ru>
2020-10-05 10:20:56 +0300

    ЛР-2 Работа с классами

---
+++ ЛР-2/Figures.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace LAB_2
+{
+    
+    public interface IPrint
+    {
+        void Print();
+    }
+
+    public abstract class Figure
+    {
+        public abstract double ploshad();
+    }
+
+    public class Rectangle : Figure, IPrint //Прямоугольник
+    {
+        protected double a, b;
+
+        public Rectangle(double A, double B)
+        {
+            a = A;
+            b = B;
+        }
+
+        public override double ploshad()
+        {
+            return a * b;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            result += "Длинна = ";
+            result += a.ToString() + " \n";
+            result += "Ширина = ";
+            result += b.ToString() + " \n";
+            result += "Площадь = ";
+            result += ploshad().ToString() + " \n";
+            return result;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+
+    public class Square : Rectangle //Квадрат
+    {
+        public Square(double A) : base(A, A)
+        {
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            result += "Сторона = ";
+            result += a.ToString() + " \n";
+            result += "Площадь = ";
+            result += ploshad().ToString() + " \n";
+            return result;
+        }
+
+        public new void Print() //new убирает предупреждение о скрытии
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+
+
+    public class Round : Figure //Круг
+    {
+        private double r;
+
+        public Round(double R)
+        {
+            r = R;
+        }
+        
+        public override double ploshad()
+        {
+            return Math.PI * r * r;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            result += "Радиус = ";
+            result += r.ToString() + " \n";
+            result += "Площадь = ";
+            result += ploshad().ToString() + " \n";
+            return result;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
---
+++ ЛР-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LAB_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Прямоугольник --------\n");
+            Rectangle r = new Rectangle(1, 2);
+            //Console.WriteLine(r);
+            r.Print();
+            Console.WriteLine("Квадрат --------\n");
+            Square s = new Square(1);
+            //Console.WriteLine(s);
+            s.Print();
+            Console.WriteLine("Круг --------\n");
+            Round o = new Round(1);
+            //Console.WriteLine(o);
+            o.Print();
+        }
+    }
+}
