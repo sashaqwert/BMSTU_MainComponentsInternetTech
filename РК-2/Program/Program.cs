fbf1d42833f33c390cb7e065b7a83c9b321ea709
Alexandr <admin@sccraft.ru>
2020-12-13 15:21:59 +0300

    РК-2
    РК-2 с XML и CS файлами

---
+++ РК-2/Program/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace RK_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLatrTest obj = new XMLatrTest();
+            obj.var1 = 12345;
+            obj.var2 = 12.34;
+            obj.var3 = true;
+            obj.var4 = "Это самая обычная строка!";
+            // Текущий каталог
+            string currentPath = "A:\\sasha\\YandexDisk\\YandexDisk\\МГТУ\\5-й семестр\\Основные компоненты интернет-технологий\\РК\\2";
+
+            //+++++++++++++++++
+            //сериализация //+++++++++++++++++
+            Console.WriteLine("До сериализации:");
+            Console.WriteLine(obj);
+            //Формирование имени файла
+            string fileXml2Name = Path.Combine(currentPath, "file2atr.xml");
+            //При сериализации файл необходимо открыть с использованием потоков
+            Stream TestFileStream1Xml2 = File.Create(fileXml2Name);
+            //Создание объекта класса сериализации
+            XmlSerializer serializerXml2 = new XmlSerializer(typeof(XMLatrTest));
+            //Cериализация объекта в файл
+            serializerXml2.Serialize(TestFileStream1Xml2, obj);
+            //Закрытие потока
+            TestFileStream1Xml2.Close();
+            //+++++++++++++++++
+            //десериализация //+++++++++++++++++
+            //Открытие файла в виде потока на чтение
+            Stream TestFileStream2Xml2 = File.OpenRead(fileXml2Name);
+            //Десериализация объекта
+            XMLatrTest dataXml2Out = (XMLatrTest)serializerXml2.Deserialize(TestFileStream2Xml2);
+            //Закрытие потока
+            TestFileStream2Xml2.Close();
+            Console.WriteLine("После десериализации:");
+            Console.WriteLine(dataXml2Out);
+        }
+    }
+}
---
+++ РК-2/Program/XMLatrTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace RK_2
+{
+    [XmlRoot(ElementName = "RK2Class")]
+    public class XMLatrTest
+    {
+        [XmlIgnore]
+        public int var1;
+
+        [XmlAttribute(AttributeName = "NumberWitchDotPoint")]
+        public double var2;
+
+        [XmlAttribute(AttributeName = "ZeroOrOne")]
+        public bool var3;
+
+        [XmlAttribute(AttributeName = "TheSring")]
+        public string var4;
+
+        public override string ToString()
+        {
+            return var1 + "\n" + var2 + "\n" + var3 + "\n" + var4 + "\n";
+        }
+    }
+}
